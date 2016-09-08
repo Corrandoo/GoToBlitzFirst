bf13e93613814fbd9fcfd8a0d67034b9e40d9b3c
Corrandoo <ivan260302@yandex.ru>
2016-09-08 19:52:29 +0500

    Some shit

--- src/main/java/me/corrandoo/Blitz.java
+++ src/main/java/me/corrandoo/Blitz.java
@@ -5,10 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Blitz {
-    static User[] topUsers = new User[10];
+    static List<User> topUsers = new ArrayList<User>();
     static List<Event> events = new ArrayList<Event>();
     static List<Step> steps = new ArrayList<Step>();
     static List<User> users = new ArrayList<User>();
+
+    public static void main(String[] args) {
+        eventsFileToList("src/main/resources/course-217-events.csv");
+        structureFileToList("src/main/resources/course-217-structure.csv");
+        getUsersList();
+        getTopUsers();
+        for(User user : topUsers){
+            System.out.println(user.getId());
+        }
+    }
     public static void eventsFileToList(String fileName){
         try {
             BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
@@ -63,6 +73,29 @@ public class Blitz {
             }
             if(isTrue)
                 users.add(new User(events.get(i).getUserId()));
+        }
+    }
+    public static void getTopUsers(){
+        Step step;
+        Event event;
+        for (int i = 0; i < events.size(); i++) {
+            event = events.get(events.size() - i);
+            for (int j = 0; j < steps.size(); j++) {
+                if(event.getStepId() == steps.get(j).getStepId()){
+                    step = steps.get(j);
+                    for (int k = 0; k < users.size(); k++) {
+                        if(event.getUserId() == users.get(k).getId()){
+                            users.get(k).setScore(users.get(k).getScore() + step.getStepCost());
+                            if(users.get(k).getScore() >= 24){
+                                topUsers.add(users.get(k));
+                            }
+                            if(topUsers.size() > 10)
+                                break;
+                        }
+                    }
+                }
+
+            }
         }
     }
 
