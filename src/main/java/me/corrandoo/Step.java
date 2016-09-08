44110ab860d2e1ee3a6eb686b07ba6dafed6c25f
Corrandoo <ivan260302@yandex.ru>
2016-09-08 19:57:43 +0500

    Some shit

---
+++ src/main/java/me/corrandoo/Step.java
@@ -0,0 +1,20 @@
+package me.corrandoo;
+
+public class Step {
+    private int stepId;
+    private int stepCost;
+
+    public Step(int stepId, int stepCost) {
+        this.stepId = stepId;
+        this.stepCost = stepCost;
+    }
+
+    public int getStepId() {
+        return stepId;
+    }
+
+    public int getStepCost() {
+        return stepCost;
+    }
+    ///
+}
